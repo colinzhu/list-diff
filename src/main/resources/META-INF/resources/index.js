4b9476198d542d91f84989b51e669edb6bfed0fd
colinzhu <colin.x.z.zhu+github@gmail.com>
2022-05-07 10:54:58 +0800

    Add logic and update layout

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -4,10 +4,14 @@ class Main {
 
     go() {
         console.log("go");
-        let leftList = document.getElementById("left").value.split("\n");
-        let rightList = document.getElementById("right").value.split("\n");
-        console.log(leftList.join("\n"));
-        console.log(rightList.join("\n"));
+        const leftOri = [...new Set(document.getElementById("ori-left").value.split("\n").sort())];
+        const rightOri = [...new Set(document.getElementById("ori-right").value.split("\n").sort())];
+
+        let leftOnly = leftOri.filter(item => rightOri.indexOf(item) == -1);
+        let rightOnly = rightOri.filter(item => leftOri.indexOf(item) == -1);
+
+        document.getElementById("result-left").value = leftOnly.join("\n");
+        document.getElementById("result-right").value = rightOnly.join("\n");
     }
 }
 let main = new Main();
